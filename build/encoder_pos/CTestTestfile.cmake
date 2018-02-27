c0dcecc557e188feb764959a5b493581e01a79d4
Haresh Miriyala <haresh.miriyala@gmail.com>
2018-02-26 22:54:50 -0600

    created the ros workspace for intense , the tensegrity based manipulator.

---
+++ build/encoder_pos/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/tensegros/src/encoder_pos
+# Build directory: /home/pi/tensegros/build/encoder_pos
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
